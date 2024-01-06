42ca76c002dfc8911b233fcff464d2a494492033
Muzammil <creategamer10@gmail.com>
2024-01-06 20:59:05 +0530

    Adduserand video models

---
+++ src/index.js
@@ -0,0 +1,70 @@
+//database not be used in single line remember
+// require('dotenv').config({path:'./env'})
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is runing at port::${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connected fails!!!!!",err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app=express()
+
+//most of time we use ; before start iife because editor bychange mistake the ; 
+(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)    
+        app.on("error",(error)=>{
+            console.log("ERROR::",error);
+            throw error
+        })   
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.log("error:",error);
+        throw err
+    }
+})()
+*/
